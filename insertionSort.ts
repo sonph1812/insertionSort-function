ffbebe15316913616024f03fa36f31a380195d35
sonph <sonph>
2022-06-17 08:41:18 +0700

    function

---
+++ insertionSort.ts
@@ -0,0 +1,16 @@
+function insertionSort(inputArr:number[]){
+    for(let i=1;i<inputArr.length;i++){
+        let current = inputArr[i];
+        let j =i-1
+while((j>-1) &&(current<inputArr[j])){
+    inputArr[j+1]=inputArr[j];
+    j--;
+
+        }
+        inputArr[j+1]=current;
+    }
+    return inputArr;
+
+}
+let inputArr = [1,3,2,4,6,9,8,0,7,5]
+console.log(insertionSort(inputArr))
